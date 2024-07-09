d234565494a290360ac0c02de5f145d3aaa08156
AbdoullahBougataya <sience.story@gmail.com>
2024-07-09 15:20:16 +0000

    Tue, Jul 9, 2024, 6:20 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,6 @@
 import wikipedia
 
 section = wikipedia.WikipediaPage('Comparison of orbital rocket engines').section('Current, Upcoming, and In-Development rocket engines')
+
+section = section.replace('\n', '').replace("\'", "")
+print(section)
