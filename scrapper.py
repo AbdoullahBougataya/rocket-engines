1e92818b2ab3eade5174aa74364b1d6eb5cecb5e
AbdoullahBougataya <sience.story@gmail.com>
2024-07-09 16:22:49 +0000

    Tue, Jul 9, 2024, 7:22 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -5,5 +5,5 @@ html = requests.get('https://en.wikipedia.org/wiki/Comparison_of_orbital_rocket_
 soup = BeautifulSoup(html.text, 'html.parser')
 table = soup.find_all('table')[0]
 rows = table.find_all('tr')
-for row in range(len(rows)):
-    
+for row in range(len(rows) - 1):
+    print(rows[row + 1])
