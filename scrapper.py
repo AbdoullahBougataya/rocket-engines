f472d066c8b1d4e92380c528b0fe23bb5866fc64
AbdoullahBougataya <sience.story@gmail.com>
2024-07-10 08:59:33 +0000

    Wed, Jul 10, 2024, 11:59 AM +03:00

--- scrapper.py
+++ scrapper.py
@@ -4,17 +4,40 @@ import re
 
 html = requests.get('https://en.wikipedia.org/wiki/Comparison_of_orbital_rocket_engines')
 soup = BeautifulSoup(html.text, 'html.parser')
-titles_bs = soup.find_all('th')
+titles_bs = soup.find_all('table')[0].find_all('th')
 titles = []
 for i in range(len(titles_bs)):
     title = titles_bs[i].get_text()
     titles.append(re.sub("\[.*?\]","[]",title.replace("\n", "").replace("\u200b", "").replace("  ", " ")).replace("[]", ""))
 rows_bs = soup.find_all('table')[0].find_all('tr')
-rows = []
+dict = []
 for i in range(len(rows_bs)):
     element = rows_bs[i].find_all('td')
     dictionary = {}
     for j in range(len(element)):
-        dictionary[titles[j]] = re.sub("\[.*?\]","[]", element[j].get_text().replace("\n", "").replace("\u200b", "").replace("\u2009", "").replace("\xa0", "").replace("  ", " ")).replace("[]", "")
-    rows.append(dictionary)
-print(rows)
+        string_element = re.sub("\[.*?\]","[]", element[j].get_text().replace("\n", "").replace("\u200b", "").replace("\u2009", "").replace("\xa0", "").replace("  ", " ")).replace("[]", "").replace(",", "")
+        if string_element.replace(".", "").isnumeric():
+            dictionary[titles[j]] = float(string_element)
+        else:
+            dictionary[titles[j]] = string_element
+    if dictionary != {}:
+        dict.append(dictionary)
+titles_bs = soup.find_all('table')[1].find_all('th')
+titles = []
+for i in range(len(titles_bs)):
+    title = titles_bs[i].get_text()
+    titles.append(re.sub("\[.*?\]","[]",title.replace("\n", "").replace("\u200b", "").replace("  ", " ")).replace("[]", ""))
+rows_bs = soup.find_all('table')[1].find_all('tr')
+dict = []
+for i in range(len(rows_bs)):
+    element = rows_bs[i].find_all('td')
+    dictionary = {}
+    for j in range(len(element)):
+        string_element = re.sub("\[.*?\]","[]", element[j].get_text().replace("\n", "").replace("\u200b", "").replace("\u2009", "").replace("\xa0", "").replace("  ", " ")).replace("[]", "").replace(",", "")
+        if string_element.replace(".", "").isnumeric():
+            dictionary[titles[j]] = float(string_element)
+        else:
+            dictionary[titles[j]] = string_element
+    if dictionary != {}:
+        dict.append(dictionary)
+print(dict)
