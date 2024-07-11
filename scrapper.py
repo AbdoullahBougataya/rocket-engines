14ac9304a3e196cbf688b686372f027cbc5f9769
AbdoullahBougataya <sience.story@gmail.com>
2024-07-11 02:50:42 +0000

    Thu, Jul 11, 2024, 5:50 AM +03:00

--- scrapper.py
+++ scrapper.py
@@ -48,7 +48,7 @@ def Wikipedia(engine_name = ""):
                     dictionary['Thrust SL (N)'] = (float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[1])) / 2
                 else:
                     dictionary['Thrust SL (N)'] = float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1])
-            elif string_element.replace("(SL)", "").replace("–", "").replace("with fuel", "").replace(".", "").isnumeric():
+            elif string_element.replace("(SL)", "").strip().replace("–", "").replace("with fuel", "").replace(".", "").isnumeric():
                 if string_element.find('–') != -1:
                     dictionary[titles[j]] = (float(string_element.replace("(SL)", "").replace("with fuel", "").split('–')[0]) + float(string_element.replace("(SL)", "").replace("with fuel", "").split('–')[1])) / 2
                 else:
@@ -99,7 +99,7 @@ def Wikipedia(engine_name = ""):
                     dictionary['Thrust SL (N)'] = (float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[1])) / 2
                 else:
                     dictionary['Thrust SL (N)'] = float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1])
-            elif string_element.replace("(SL)", "").replace("with fuel", "").replace("–", "").replace(".", "").isnumeric():
+            elif string_element.replace("(SL)", "").strip().replace("with fuel", "").replace("–", "").replace(".", "").isnumeric():
                 if string_element.find('–') != -1:
                     dictionary[titles[j]] = (float(string_element.replace("(SL)", "").replace("with fuel", "").split('–')[0]) + float(string_element.replace("(SL)", "").replace("with fuel", "").split('–')[1])) / 2
                 else:
