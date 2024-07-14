5bbd289c0f401b5e53ddf5df6925e3a37c6e0579
AbdoullahBougataya <sience.story@gmail.com>
2024-07-14 09:14:29 +0000

    Sun, Jul 14, 2024, 12:14 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -132,6 +132,8 @@ def Wikipedia():
             else:
                 # Assigning the string element from the table to a value in the dictionary
                 dictionary[titles[j]] = string_element
+            if i == 0:
+                datatypes.append()
         # Append the list dict with the dictionary and add an status of retired if it is not empty
         if dictionary != {}:
             dictionary["Status"] = "Retired"
