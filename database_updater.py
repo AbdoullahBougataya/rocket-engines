4ae46f5f377d69dc55116070cc790bf46d445875
AbdoullahBougataya <sience.story@gmail.com>
2024-07-14 07:53:12 +0000

    Sun, Jul 14, 2024, 10:53 AM +03:00

--- database_updater.py
+++ database_updater.py
@@ -1,10 +1,18 @@
+# Importing Sqlite3 Module
 import sqlite3
 
-# filename to form database
-file = "db\database.db"
-
 try:
-    conn = sqlite3.connect(file)
-    print("Database db\database.db formed.")
-except:
-    print("Database db\database.db not formed.")
+	# Making a connection between sqlite3 database and Python Program
+	sqliteConnection = sqlite3.connect('db\database.db')
+	# If sqlite3 makes a connection with python program then it will print "Connected to SQLite"
+	# Otherwise it will show errors
+	print("Connected to SQLite")
+except sqlite3.Error as error:
+	print("Failed to connect with the sqlite3 database", error)
+finally:
+	# Inside Finally Block, If connection is open, we need to close it
+	if sqliteConnection:
+		# using close() method, we will close the connection
+		sqliteConnection.close()
+		# After closing connection object, we will print "the sqlite connection is closed"
+		print("the sqlite connection is closed")
