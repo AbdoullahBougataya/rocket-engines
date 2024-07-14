cb4d2f60c877270e05206aaa1ef0f8ed14d2939a
AbdoullahBougataya <sience.story@gmail.com>
2024-07-14 09:44:24 +0000

    Sun, Jul 14, 2024, 12:44 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import sys
+import sqlite3
 
 # scrapper() is a function that scrape the elements of the list that contains the informations about rocket engines. This function return a dictionary that contains those elements.
 def scrapper():
@@ -140,3 +140,24 @@ def scrapper():
     # Handle the function parameter
     return dict, titles, datatypes
 
+try:
+	# Making a connection between sqlite3 database and Python Program
+	con = sqlite3.connect('db\database.db')
+	# If sqlite3 makes a connection with python program then it will print "Connected to SQLite"
+	# Otherwise it will show errors
+	print("Connected to SQLite")
+except sqlite3.Error as error:
+	print("Failed to connect with the sqlite3 database", error)
+finally:
+	# Inside Finally Block, If connection is open
+	if con:
+		cur = con.cursor()
+		cur.execute("CREATE TABLE engines (id INT)")
+		engines, titles, datatype = scrapper()
+		for i in titles:
+		    cur.execute(f"ALTER TABLE engines ADD '{i}' {datatype}")
+		cur.execute("SELECT * FROM engines")
+		# using close() method, we will close the connection
+		con.close()
+		# After closing connection object, we will print "the sqlite connection is closed"
+		print("the sqlite connection is closed")
