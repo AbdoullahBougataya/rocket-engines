59d382e2c260c35f5b2f705d3528f3ccf933a2cc
AbdoullahBougataya <sience.story@gmail.com>
2024-07-14 09:30:07 +0000

    Sun, Jul 14, 2024, 12:30 PM +03:00

--- database_updater.py
+++ database_updater.py
@@ -18,7 +18,7 @@ finally:
 		cur.execute("CREATE TABLE engines")
 		for i in titles:
 		    cur.execute(f"ALTER TABLE engines ADD '{i}' {datatype}")
-		cur.execute("CREATE TABLE engines")
+		cur.execute("SELECT * FROM engines")
 		# using close() method, we will close the connection
 		con.close()
 		# After closing connection object, we will print "the sqlite connection is closed"
