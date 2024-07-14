d5f980c465bd5748e6464b6dbd5974c06da93ac5
AbdoullahBougataya <sience.story@gmail.com>
2024-07-14 10:38:30 +0000

    Sun, Jul 14, 2024, 1:38 PM +03:00

---
+++ database_updater.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+con = sqlite3.connect('db\database.db')
+
+# cursor object
+cur = con.cursor()
+
+# Drop the GEEK table if already exists.
+cur.execute("DROP TABLE IF EXISTS ENGINES")
+
+engines = scrapper()
+
+# Creating table
+table = """ CREATE TABLE ENGINES (
+            Engine TEXT NOT NULL,
+            Origin TEXT NOT NULL,
+            Designer TEXT,
+            Vehicle TEXT,
+            Status TEXT NOT NULL,
+            Use TEXT,
+            Propellant TEXT,
+            'Power cycle' TEXT,
+            'Specific impulse (s)' REAL,
+            'Thrust (N)' REAL,
+            'Chamber pressure (bar)' REAL,
+            'Mass (kg)' TEXT,
+            'Thrust weight ratio' REAL,
+            'Oxidiser:fuel ratio' REAL
+        ); """
+
+cur.execute(table)
+
+print("Table is Ready")
+
+# Close the connection
+con.close()
