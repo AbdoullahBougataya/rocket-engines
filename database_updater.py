d736cbc3cd9ce8620a7650405e6c54c70e4846a6
AbdoullahBougataya <sience.story@gmail.com>
2024-07-14 08:30:59 +0000

    Sun, Jul 14, 2024, 11:30 AM +03:00

--- database_updater.py
+++ database_updater.py
@@ -3,7 +3,7 @@ import sqlite3
 
 try:
 	# Making a connection between sqlite3 database and Python Program
-	sqliteConnection = sqlite3.connect('db\database.db')
+	con = sqlite3.connect('db\database.db')
 	# If sqlite3 makes a connection with python program then it will print "Connected to SQLite"
 	# Otherwise it will show errors
 	print("Connected to SQLite")
@@ -11,9 +11,9 @@ except sqlite3.Error as error:
 	print("Failed to connect with the sqlite3 database", error)
 finally:
 	# Inside Finally Block, If connection is open
-	if sqliteConnection:
-		
+	if con:
+		cur = con.cursor()
 		# using close() method, we will close the connection
-		sqliteConnection.close()
+		con.close()
 		# After closing connection object, we will print "the sqlite connection is closed"
 		print("the sqlite connection is closed")
