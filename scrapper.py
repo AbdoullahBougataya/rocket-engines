a74e045cf42474cf816bc772fd051b429499672f
AbdoullahBougataya <sience.story@gmail.com>
2024-07-17 15:17:15 +0000

    Wed, Jul 17, 2024, 6:17 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -15,13 +15,13 @@ def scrapper():
     for i in range(len(titles_bs)):
         title = re.sub("\[.*?\]","[]",titles_bs[i].get_text().replace("\n", "").replace("\u200b", "").replace("  ", " ")).replace("[]", "")
         if title == 'Specific impulse (s)':
-            titles = titles + tuple('Specific impulse Vac (s)')
-            titles = titles + tuple('Specific impulse SL (s)')
+            titles = titles + tuple(map(str, ['Specific impulse Vac (s)']))
+            titles = titles + tuple(map(str, ['Specific impulse SL (s)']))
         elif title == 'Thrust (N)':
-            titles = titles + tuple('Thrust Vac (s)')
-            titles = titles + tuple('Thrust SL (s)')
+            titles = titles + tuple(map(str, ['Thrust Vac (s)']))
+            titles = titles + tuple(map(str, ['Thrust SL (s)']))
         else:
-            titles = titles + tuple(title)
+            titles = titles + tuple(map(str, [title]))
     # get the table rows
     # rows_bs = soup.find_all('table')[0].find_all('tr')
     # dict = []
