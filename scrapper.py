b24c8bb8b24d8e7a4d4ddb50f0c106fe9813b0e8
AbdoullahBougataya <sience.story@gmail.com>
2024-07-18 17:04:03 +0000

    Thu, Jul 18, 2024, 8:04 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -42,7 +42,7 @@ def scrapper():
                     row += tuple(map(float, [(float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0].split('–')[1])) / 2, (float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[1])) / 2]))
                 else:
                     row += tuple(map(float, [float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0]), float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1])]))
-            elif titles[j] == 'Specific impulse Vac (s)' and re.findall(r'\(.*?\)', string_element)[0] == '(Vac)':
+            elif titles[j] == 'Specific impulse Vac (s)' and re.findall(r'\(.*?\)', string_element) == '(Vac)':
                 row += tuple(map(float, [float(string_element.split()[0])]))
             # Split the thrust column into Thrust in the vaccum and Thrust in the sea level
             elif titles[j] == 'Specific impulse SL (s)' and len(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()) > 1:
