d8ee40a110a8fe68c9efdc0aa3d9216334dd0a5e
AbdoullahBougataya <sience.story@gmail.com>
2024-07-18 14:50:22 +0000

    Thu, Jul 18, 2024, 5:50 PM +03:00

--- database_updater.py
+++ database_updater.py
@@ -15,32 +15,16 @@ titles, engines = scrapper()
 table = """ CREATE TABLE ENGINES (Id INTEGER NOT NULL, """
 for i in range(len(titles)):
     table += titles[i]
-    if typeof(engines[7][i]) == str:
+    if type(engines[7][i]) == str:
         table += """ TEXT, """
-    elif typeof(engines[7][i]) == float:
+    elif type(engines[7][i]) == float:
         table += """ REAL, """
+table = table.removesuffix(""", """)
+table += """);"""
+print(table)
+# cur.execute(table)
 
-            Engine TEXT NOT NULL,
-            Origin TEXT NOT NULL,
-            Designer TEXT,
-            Vehicle TEXT,
-            Status TEXT NOT NULL,
-            Use TEXT,
-            Propellant TEXT,
-            'Power cycle' TEXT,
-            'Specific impulse Vac (s)' REAL,
-            'Specific impulse SL (s)' REAL,
-            'Thrust Vac (N)' REAL,
-            'Thrust SL (N)' REAL,
-            'Chamber pressure (bar)' REAL,
-            'Mass (kg)' TEXT,
-            'Thrust weight ratio' REAL,
-            'Oxidiser:fuel ratio' REAL
-        ); """
-
-cur.execute(table)
-
-print("Table is Ready")
+# print("Table is Ready")
 
 # Close the connection
 con.close()
