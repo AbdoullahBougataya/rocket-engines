d58fea290f47a7ee09504c97d26fc66c671bdb03
AbdoullahBougataya <sience.story@gmail.com>
2024-07-19 14:47:44 +0000

    Fri, Jul 19, 2024, 5:47 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -118,7 +118,7 @@ def scrapper():
                 else:
                     row += tuple(map(float, [float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0]), float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1])]))
             # Change numbers from strings to floats
-            if j == len(element) and i == 113:
+            if titlesr[j] == 'titlesr[j] != 'Thrust Vac (N)'' and i == 115:
                 print(string_element.replace("(SL)", "").strip().replace("–", "").replace(".", "").isnumeric())
             elif string_element.replace("(SL)", "").strip().replace("–", "").replace(".", "").isnumeric() and titlesr[j] != 'Thrust Vac (N)':
                 # Change intervals to medians
