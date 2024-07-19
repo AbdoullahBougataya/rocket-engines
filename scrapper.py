afc8c6db0855ce74bf85f25db4a8a837aca34e15
AbdoullahBougataya <sience.story@gmail.com>
2024-07-19 13:44:25 +0000

    Fri, Jul 19, 2024, 4:44 PM +03:00

--- scrapper.py
+++ scrapper.py
@@ -41,9 +41,9 @@ def scrapper():
             if titles[j] == 'Specific impulse Vac (s)' and len(re.sub("\(.*?\)","()", string_element).replace("()", "").replace(",", "").replace("  ", " ").split()) > 1:
                 # Change the Isps intervals to medians
                 if string_element.find('–') != -1:
-                    row += tuple(map(float, [(float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0].split('–')[1])) / 2, (float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1].split('–')[1])) / 2]))
+                    row += tuple(map(float, [(float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").replace(",", "").split()[0].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").replace(",", "").split()[0].split('–')[1])) / 2, (float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").replace(",", "").split()[1].split('–')[0]) + float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").replace(",", "").split()[1].split('–')[1])) / 2]))
                 else:
-                    row += tuple(map(float, [float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[0]), float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").split()[1])]))
+                    row += tuple(map(float, [float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").replace(",", "").split()[0]), float(re.sub("\(.*?\)","()", string_element).replace("()", "").replace("  ", " ").replace(",", "").split()[1])]))
             elif titles[j] == 'Specific impulse Vac (s)' and string_element[string_element.find("(")+1:string_element.find(")")] == 'Vac':
                 row += tuple(map(float, [float(string_element.split(" ")[0])]))
                 row += tuple(map(str, ['']))
