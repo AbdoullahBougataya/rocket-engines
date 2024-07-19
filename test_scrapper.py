b12792211277a4be48b8b7159b4287c17984b04b
AbdoullahBougataya <sience.story@gmail.com>
2024-07-19 15:23:08 +0000

    Fri, Jul 19, 2024, 6:23 PM +03:00

--- test_scrapper.py
+++ test_scrapper.py
@@ -3,9 +3,11 @@ import scrapper
 
 class TestScrapper(unittest.TestCase):
 
-    def test_scrapper(self):
-        titles, data = scrapper()
+    def test_titles(self):
+        titles = scrapper()[0]
         self.assertEqual(titles, ('Engine', 'Origin', 'Designer', 'Vehicle', 'Status', 'Use', 'Propellant', 'Power cycle', 'Specific impulse Vac (s)', 'Specific impulse SL (s)', 'Thrust Vac (N)', 'Thrust SL (N)', 'Chamber pressure (bar)', 'Mass (kg)', 'Thrust:weight ratio', 'Oxidiser:fuel ratio'))
+    def test_data(self):
+        data = scrapper()[1]
         for i in range(len(data) - 1):
             self.assertEqual(len(data[i]), 16)
 
