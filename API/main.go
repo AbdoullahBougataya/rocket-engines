71fa515a753002773b47ebbf55a0f84ad3720ca2
AbdoullahBougataya <sience.story@gmail.com>
2024-07-20 17:37:26 +0000

    Sat, Jul 20, 2024, 8:37 PM +03:00

--- API/main.go
+++ API/main.go
@@ -4,14 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/mattn/go-sqlite3"
-	"net/http"
-	"github.com/gin-gonic/gin"
-	"errors"
 )
 
 func main()
 {
-	database, err = sql.Open("sqlite3", "../db/database.db");
+	database, err = sql.Open("sqlite3", "../db/database.db")
 	if err != nil {
         fmt.Println("Error opening database:", err)
         return
