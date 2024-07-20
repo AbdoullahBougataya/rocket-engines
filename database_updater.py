186e4ff69ffa886527bbda3ea3a1aa2deb2944fa
AbdoullahBougataya <sience.story@gmail.com>
2024-07-20 11:49:54 +0000

    Sat, Jul 20, 2024, 2:49 PM +03:00

--- database_updater.py
+++ database_updater.py
@@ -6,24 +6,21 @@ con = sqlite3.connect('db\database.db')
 # cursor object
 cur = con.cursor()
 
-# Drop the GEEK table if already exists.
-cur.execute("DROP TABLE IF EXISTS rocket_engines")
-
 titles, engines = scrapper()
 
 # Creating table
-table = """ CREATE TABLE rocket_engines (Id INTEGER NOT NULL, """
-for i in range(len(titles)):
-    table += f"""'{titles[i]}'"""
-    if type(engines[17][i]) == str:
-        table += """ TEXT, """
-    elif type(engines[17][i]) == float:
-        table += """ REAL, """
-table = table.removesuffix(""", """)
-table += """); """
-cur.execute(table)
+# table = """ CREATE TABLE rocket_engines (Id INTEGER NOT NULL, """
+# for i in range(len(titles)):
+#     table += f"""'{titles[i]}'"""
+#     if type(engines[17][i]) == str:
+#         table += """ TEXT, """
+#     elif type(engines[17][i]) == float:
+#         table += """ REAL, """
+# table = table.removesuffix(""", """)
+# table += """); """
+# cur.execute(table)
 
-print("Table is Ready")
+# print("Table is Ready")
 
 for i in range(len(engines)):
     cur = con.cursor()
