fa8c5173f53c2a90c9c170696ef345dafe1ca191
AbdoullahBougataya <sience.story@gmail.com>
2024-07-20 14:28:38 +0000

    Sat, Jul 20, 2024, 5:28 PM +03:00

--- database_updater.py
+++ database_updater.py
@@ -6,33 +6,30 @@ con = sqlite3.connect('db\database.db')
 # cursor object
 cur = con.cursor()
 
-# # Drop the rocket_engines table if already exists.
-# cur.execute("DROP TABLE IF EXISTS rocket_engines")
+# Drop the rocket_engines table if already exists.
+cur.execute("DROP TABLE IF EXISTS rocket_engines")
 
 titles, engines = scrapper()
 
 # Creating table
-# table = """ CREATE TABLE rocket_engines (Id INTEGER NOT NULL, """
-# for i in range(len(titles)):
-#     table += f"""'{titles[i]}'"""
-#     if type(engines[17][i]) == str:
-#         table += """ TEXT, """
-#     elif type(engines[17][i]) == float:
-#         table += """ REAL, """
-# table = table.removesuffix(""", """)
-# table += """); """
-# cur.execute(table)
+table = """ CREATE TABLE rocket_engines (Id INTEGER NOT NULL, """
+for i in range(len(titles)):
+    table += f"""'{titles[i]}'"""
+    if type(engines[17][i]) == str:
+        table += """ TEXT, """
+    elif type(engines[17][i]) == float:
+        table += """ REAL, """
+table = table.removesuffix(""", """)
+table += """); """
+cur.execute(table)
 
-# print("Table is Ready")
+print("Table is Ready")
 
 for i in range(len(engines)):
     cur = con.cursor()
-    cur.execute("SELECT Engine FROM rocket_engines WHERE Engine = ?", (engines[i][0],))
-    data = cur.fetchall()
-    if len(data)==0:
-        cur.execute("INSERT INTO rocket_engines VALUES ?;".replace("''", "NULL"), (i, ) + engines[i])
+    cur.execute("INSERT INTO rocket_engines VALUES ?;".replace("''", "NULL"), (i, ) + engines[i])
     con.commit()
 
-print("Table successfully updated")
+print("Table successfully filled")
 # Close the connection
 con.close()
