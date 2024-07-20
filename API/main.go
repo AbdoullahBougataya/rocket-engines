15c91288b7e19007ad8f5de4197dc0480dc4347b
AbdoullahBougataya <sience.story@gmail.com>
2024-07-20 15:32:22 +0000

    Sat, Jul 20, 2024, 6:32 PM +03:00

--- API/main.go
+++ API/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/mattn/go-sqlite3"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"errors"
 )
 
-
+func main()
+{
+	database, _ = sql.Open("sqlite3", "../db/database.db");
+}
