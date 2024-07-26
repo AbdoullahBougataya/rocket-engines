47251db6991eaa5ef9e361ca027aea078f910ac0
AbdoullahBougataya <sience.story@gmail.com>
2024-07-26 16:19:36 +0000

    Fri, Jul 26, 2024, 7:19 PM +03:00

--- API/main.go
+++ API/main.go
@@ -12,18 +12,20 @@ type Vacsl struct {
     SL    float64    `json:"surface_level"`
 }
 type Record struct {
-    Id               int     `json:"id"`
-    Engine           string  `json:"engine"`
-    Origin           string  `json:"origin"`
-    Designer         string  `json:"designer"`
-    Vehicle          string  `json:"vehicle"`
-    Status           string  `json:"status"`
-    Use              string  `json:"use"`
-    Propellant       string  `json:"propellant"`
-    Power_cycle      string  `json:"power_cycle"`
-    Isp              Vacsl   `json:"specific_impulse_(s)"`
-    Thrust           Vacsl   `json:"thrust_(N)"`
-    Chamber_pressure float64 `json:"chamber_pressure_(bar)"`
+    Id                  int     `json:"id"`
+    Engine              string  `json:"engine"`
+    Origin              string  `json:"origin"`
+    Designer            string  `json:"designer"`
+    Vehicle             string  `json:"vehicle"`
+    Status              string  `json:"status"`
+    Use                 string  `json:"use"`
+    Propellant          string  `json:"propellant"`
+    Power_cycle         string  `json:"power_cycle"`
+    Isp                 Vacsl   `json:"specific_impulse_(s)"`
+    Thrust              Vacsl   `json:"thrust_(N)"`
+    Chamber_pressure    float64 `json:"chamber_pressure_(bar)"`
+    Mass                string  `json:"mass_(kg)"`
+    Thrust_weight_ratio float64 `json:"thrust:weight_ratio"`
 }
 
 func main() {
