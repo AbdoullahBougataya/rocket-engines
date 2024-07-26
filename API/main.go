480e1c229f510938f9cf91aed53a961ff4a03247
AbdoullahBougataya <sience.story@gmail.com>
2024-07-26 16:31:24 +0000

    Fri, Jul 26, 2024, 7:31 PM +03:00

--- API/main.go
+++ API/main.go
@@ -48,7 +48,7 @@ func main() {
         var engine Engine
         var isp    Vacsl
         var thrust Vacsl
-        err := rows.Scan(&engine.Id, &engine.Engine, &engine.Designer, &engine.Origin, &engine.)
+        err := rows.Scan(&engine.Id, &engine.Engine, &engine.Designer, &engine.Origin, &engine.Designer, &engine.Vehicle, &engine.Status, &engine.Use, &engine.Propellant, &engine.Power_cycle, &isp.Vac, &isp.SL, &thrust.Vac, &thrust.SL, &engine.)
         if err != nil {
             log.Fatal(err)
         }
