931db1aab6d8497cb798d729193b38433a6b06f2
AbdoullahBougataya <sience.story@gmail.com>
2024-08-02 18:13:25 +0000

    Fri, Aug 2, 2024, 9:13 PM +03:00

--- images_scrapper.py
+++ images_scrapper.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-research_later = "hiya"
-goog_search = "https://www.google.co.uk/search?sclient=psy-ab&client=ubuntu&hs=k5b&channel=fs&biw=1366&bih=648&noj=1&q=" + research_later
+research_later = "F-1"
+goog_search = "https://www.google.co.uk/search?sclient=psy-ab&client=ubuntu&hs=k5b&channel=fs&biw=1366&bih=648&noj=1&q=" + research_later.replace(" ", "+") + "+rocket+engine+wikipedia"
 
 
 r = requests.get(goog_search)
