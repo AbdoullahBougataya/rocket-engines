85b787e3bdd9b614ab9f6db443595974f1ddcc39
AbdoullahBougataya <sience.story@gmail.com>
2024-08-02 18:10:23 +0000

    Fri, Aug 2, 2024, 9:10 PM +03:00

---
+++ images_scrapper.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+research_later = "hiya"
+goog_search = "https://www.google.co.uk/search?sclient=psy-ab&client=ubuntu&hs=k5b&channel=fs&biw=1366&bih=648&noj=1&q=" + research_later
+
+
+r = requests.get(goog_search)
+
+soup = BeautifulSoup(r.text, "html.parser")
+print(soup.find('cite').text)
