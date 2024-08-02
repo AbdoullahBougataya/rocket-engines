88ac861241d9df7c4b8523e87fc2e27be919041f
AbdoullahBougataya <sience.story@gmail.com>
2024-08-02 18:55:44 +0000

    Fri, Aug 2, 2024, 9:55 PM +03:00

--- images_scrapper.py
+++ images_scrapper.py
@@ -8,5 +8,5 @@ goog_search = "https://www.google.com/search?sclient=psy-ab&client=ubuntu&hs=k5b
 r = requests.get(goog_search)
 
 soup = BeautifulSoup(r.text, "html.parser")
-search_result = soup.find_all("a", {"data-ved" : ""})
+search_result = soup.find_all("a", {"data-ved" : "2ahUKEwj21_Cf_daHAxVL1zgGHWfiDjAQFnoECAsQAg"})
 print(search_result)
