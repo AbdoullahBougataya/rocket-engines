4be49e93a907e20d8735a3dbe82be363c9cc6d84
AbdoullahBougataya <sience.story@gmail.com>
2024-08-02 20:02:10 +0000

    Fri, Aug 2, 2024, 11:02 PM +03:00

--- images_scrapper.py
+++ images_scrapper.py
@@ -20,7 +20,8 @@ for engine in engines:
         wikipedia_link = str(the_link.get('href')).removeprefix("/url?q=").split("&")[0]
         r = requests.get(wikipedia_link)
         soup = BeautifulSoup(r.text, "html.parser")
-        if soup.find_all("img")[4].get("srcset"):
-            img_data = requests.get("https:" + str(soup.find_all("img")[4].get("srcset").split()[2])).content
-            with open(f'images/{engine_name}.jpg', 'wb') as handler:
-                handler.write(img_data)
+        print(soup.find("table", {"class" : "infobox"}).find_all("img"))
+        # if soup.find("table", {"class" : "infobox"}).get("srcset"):
+        #     img_data = requests.get("https:" + str(soup.find_all("img")[4].get("srcset").split()[2])).content
+        #     with open(f'images/{engine_name}.jpg', 'wb') as handler:
+        #         handler.write(img_data)
