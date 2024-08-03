0e570e192483b031e6341623280daa202ed4d117
AbdoullahBougataya <sience.story@gmail.com>
2024-08-03 06:22:09 +0000

    Sat, Aug 3, 2024, 9:22 AM +03:00

--- images_scrapper.py
+++ images_scrapper.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import scrapper
-import os
+import os, shutil
 
+# This function download images from a given URL to a file directory
 def download_image(image_url, file_dir):
     response = requests.get(image_url, headers = {'User-Agent': 'NicoBot/0.1 (your@email.address)'})
 
@@ -17,27 +18,40 @@ def download_image(image_url, file_dir):
     else:
         print(f"Failed to download " +  image_url + " to " + file_dir + f"Status code: {response.status_code}")
 
-titles, engines = scrapper()
-for engine in engines:
-    engine_name = engine[0]
-    goog_search = "https://www.google.com/search?sclient=psy-ab&client=ubuntu&hs=k5b&channel=fs&biw=1366&bih=648&noj=1&q=" + engine_name.replace(" ", "+") + "+rocket+engine+wikipedia"
+# Clear the images file
+for filename in os.listdir("./images"):
+    file_path = os.path.join("./images", filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+# This function scrappes images of rocket engines
+def images_scrapper():
+    engines = scrapper()[1]
+    for engine in engines:
+        engine_name = engine[0]
+        goog_search = "https://www.google.com/search?sclient=psy-ab&client=ubuntu&hs=k5b&channel=fs&biw=1366&bih=648&noj=1&q=" + engine_name.replace(" ", "+") + "+rocket+engine+wikipedia"
 
 
-    r = requests.get(goog_search)
+        r = requests.get(goog_search)
 
-    soup = BeautifulSoup(r.text, "html.parser")
-    search_result = soup.find_all("a")
-    for i in range(len(search_result)):
-        if "https://en.wikipedia.org/wiki/" in str(search_result[i]):
-            the_link = search_result[i]
-            break
-    if the_link:
-        wikipedia_link = str(the_link.get('href')).removeprefix("/url?q=").split("&")[0]
-        r = requests.get(wikipedia_link)
         soup = BeautifulSoup(r.text, "html.parser")
-        if soup.find("table", {"class" : "infobox"}):
-            if soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}):
-                if soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset"):
-                    if len(soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset").split()) > 2 and "Aeon" not in engine_name:
-                        pic_url = "https:" + str(soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset").split()[2])
-                        download_image(pic_url, f"./images/{engine_name}.jpg")
+        search_result = soup.find_all("a")
+        for i in range(len(search_result)):
+            if "https://en.wikipedia.org/wiki/" in str(search_result[i]):
+                the_link = search_result[i]
+                break
+        if the_link:
+            wikipedia_link = str(the_link.get('href')).removeprefix("/url?q=").split("&")[0]
+            r = requests.get(wikipedia_link)
+            soup = BeautifulSoup(r.text, "html.parser")
+            if soup.find("table", {"class" : "infobox"}):
+                if soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}):
+                    if soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset"):
+                        if len(soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset").split()) > 2 and "Aeon" not in engine_name:
+                            pic_url = "https:" + str(soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset").split()[2])
+                            download_image(pic_url, f"./images/{engine_name}.jpg")
