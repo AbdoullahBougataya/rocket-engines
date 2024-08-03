4477b680ee9f11f6ec20e1d1b2db5bdec1c90246
AbdoullahBougataya <sience.story@gmail.com>
2024-08-03 05:17:15 +0000

    Sat, Aug 3, 2024, 8:17 AM +03:00

--- images_scrapper.py
+++ images_scrapper.py
@@ -29,6 +29,7 @@ for engine in engines:
                         pic_url = "https:" + str(soup.find("table", {"class" : "infobox"}).find("td", {"class": "infobox-image"}).find("img").get("srcset").split()[2])
                         with open(f'images/{engine_name}.jpg', 'wb') as handler:
                             response = requests.get(pic_url, stream=True)
+                            if response.method
                             for block in response.iter_content(512):
                                 if not block:
                                     break
